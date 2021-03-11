56d1011d3a00a083981da209ea1207a1d59564ed
David <zair.d@outlook.fr>
2021-03-11 18:16:18 +0100

    feat(build): update EmergencySystemApplication.java

--- src/main/java/emergencysystem/EmergencySystemApplication.java
+++ src/main/java/emergencysystem/EmergencySystemApplication.java
@@ -2,12 +2,19 @@ package emergencysystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
-public class EmergencySystemApplication {
+public class EmergencySystemApplication extends WebMvcConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmergencySystemApplication.class, args);
+	}
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addRedirectViewController("/", "/persons");
 	}
 
 }
