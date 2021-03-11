3c4e22c50b1a3397a0f24312a450b2f106ac4b24
David <zair.d@outlook.fr>
2021-03-11 18:47:53 +0100

    chore(build): add PersonRepository.java

---
+++ src/main/java/emergencysystem/util/PersonRepository.java
@@ -0,0 +1,12 @@
+package emergencysystem.util;
+
+import emergencysystem.model.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface PersonRepository extends JpaRepository<Person, Long> {
+
+    List<Person> findPerson(String person);
+
+}
