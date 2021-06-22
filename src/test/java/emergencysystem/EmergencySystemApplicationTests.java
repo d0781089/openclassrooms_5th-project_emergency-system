74a97fa290db0c5de54601666e6f3378964ea71f
David <zair.d@outlook.fr>
2021-06-22 15:10:22 +0200

    fix(build): correct defectuous Gradle exclusion

--- src/test/java/emergencysystem/EmergencySystemApplicationTests.java
+++ src/test/java/emergencysystem/EmergencySystemApplicationTests.java
@@ -1,13 +1,26 @@
 package emergencysystem;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class EmergencySystemApplicationTests {
 
+	private static final Logger logger = LogManager.getLogger(EmergencySystemApplicationTests.class);
+
 	@Test
-	void contextLoads() {
+	void shouldLogMyMessage() {
+		String myMessage = "Greetings!";
+		System.out.println(myMessage);
+
+		logger.trace("The user has just been greeted!");
+		logger.debug("The user has just been greeted!");
+		logger.info("The user has just been greeted!");
+		logger.warn("The user has just been greeted!");
+		logger.error("The user has just been greeted!");
+		logger.fatal("The user has just been greeted!");
 	}
 
 }
